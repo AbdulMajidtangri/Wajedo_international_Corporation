98c65763cfb39e914c1e8e0d5eab7126313b71e9
abdul majid <majidalitangri7@gmail.com>
2025-04-26 08:11:36 +0500

    base./

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Wajedo_international_Corporation/', // 👈 set this to your GitHub repo name with slashes
+  base: './',
   plugins: [react()],
 })
